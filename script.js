770f510bbb3a1aaea1bafd9d644141e9a641ce17
dpaginter <156012774+dpaginter@users.noreply.github.com>
2024-07-08 14:29:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#examDetails').slideDown();
+  
+
+    let projectSum = 
+    "For our final project in CIT 215, we are creating a web application that utilizes various technologies we've learned throughout the course CRUD, API, JavaScript, and jQuery. I will be making a Dungeon and Dragons' character sheet to show off all that I have learned.  This project is due Monday, April 29th."
+    $('#projectSum').text(projectSum);
+
+
+  });
